07da53dd9f51921fb355b959ebaa20fee1c8dd25
Anatoliy Spitchenko <anatolij.sp@yandex.ru>
2026-08-20 12:41:48 +0700

    fix create post

--- 07_crud/server/src/main/kotlin/ru/netology/nmedia/dto/Post.kt
+++ 07_crud/server/src/main/kotlin/ru/netology/nmedia/dto/Post.kt
@@ -1,6 +1,9 @@
 package ru.netology.nmedia.dto
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
 import ru.netology.nmedia.enumeration.AttachmentType
+import java.time.Instant
 
 data class Post(
     val id: Long,
@@ -11,7 +14,28 @@ data class Post(
     val likedByMe: Boolean,
     val likes: Int = 0,
     var attachment: Attachment? = null,
-)
+) {
+    @JsonCreator
+    constructor(
+        @JsonProperty("id") id: Long?,
+        @JsonProperty("author") author: String?,
+        @JsonProperty("authorAvatar") authorAvatar: String?,
+        @JsonProperty("content") content: String,
+        @JsonProperty("published") published: Instant?,
+        @JsonProperty("likedByMe") likedByMe: Boolean?,
+        @JsonProperty("likes") likes: Int?,
+        @JsonProperty("attachment") attachment: Attachment?,
+    ) : this(
+        id = id ?: 0,
+        author = author ?: "",
+        authorAvatar = authorAvatar.orEmpty(),
+        content = content,
+        published = (published ?: Instant.now()).toEpochMilli(),
+        likedByMe = likedByMe ?: false,
+        likes = likes ?: 0,
+        attachment = attachment,
+    )
+}
 
 data class Attachment(
     val url: String,
